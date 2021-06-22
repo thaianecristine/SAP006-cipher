e1641494d0fce1eb748bac9afbcc5a350d399816
thaiane-santos <thaiane_cristine@hotmail.com>
2021-06-22 13:51:35 -0300

    Finalização com readme para realizar o deploy

--- src/cipher.js
+++ src/cipher.js
@@ -8,24 +8,20 @@ const cipher = {
     for (let i = 0; i < criptografarText.length; i++) {
       let codigoASC = criptografarText.charCodeAt(i);
 
-      //NUMEROS//
       if (codigoASC >= 34 && codigoASC <= 63) {
         codigoASC = ((codigoASC - 34 + offset) % 29) + 34;
         resultado += String.fromCharCode(codigoASC)
 
       }
-      //MAUSCULA//
       else if (codigoASC >= 65 && codigoASC <= 90) {
         codigoASC = ((codigoASC - 65 + offset) % 26) + 65;
         resultado += String.fromCharCode(codigoASC)
 
       }
-      // minuscula//
       else if (codigoASC >= 97 && codigoASC <= 122) {
         codigoASC = ((codigoASC - 97 + offset) % 26) + 97;
         resultado += String.fromCharCode(codigoASC)
       }
-      //acentuação//
       else if (codigoASC >= 123 && codigoASC <= 254) {
         codigoASC = ((codigoASC - 123 + offset) % 131) + 123;
         resultado += String.fromCharCode(codigoASC)
